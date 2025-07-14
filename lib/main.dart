88c14150584ee5ee11007db39bae515599c5c182
Ecky819 <marcoeggert73@gmail.com>
2025-07-14 11:12:53 +0200

    Readme gelöscht

--- lib/main.dart
+++ lib/main.dart
@@ -7,14 +7,12 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         backgroundColor: const Color.fromRGBO(233, 229, 221, 1.0),
         appBar: PreferredSize(
-          // ignore: sort_child_properties_last
           preferredSize: Size.fromHeight(56.0),
           child: AppBar(
             backgroundColor: const Color.fromRGBO(40, 58, 73, 1.0),
@@ -22,12 +20,10 @@ class MyApp extends StatelessWidget {
             centerTitle: false,
             titleSpacing: 0,
             title: const Padding(
-              padding: EdgeInsets.all(
-                0.0,
-              ), // Optional: Etwas Abstand um das Bild
+              padding: EdgeInsets.all(0.0),
               child: Image(
                 image: AssetImage('assets/images/Logo.png'),
-                height: 56, // Optional: Höhe des Bildes anpassen
+                height: 56,
               ),
             ),
             actions: const [
@@ -40,120 +36,126 @@ class MyApp extends StatelessWidget {
         ),
         body: Center(
           child: ListView(
-            padding: const EdgeInsets.fromLTRB(80, 16, 80, 8),
+            padding: const EdgeInsets.fromLTRB(16, 16, 16, 16),
             children: [
-              SizedBox(
-                width: 204,
-                height: 250,
-                child: Card(
-                  color: Color.fromRGBO(243, 239, 231, 1.0),
-                  elevation: 10,
-                  margin: EdgeInsets.fromLTRB(16, 4, 16, 8),
+              Center(
+                child: ConstrainedBox(
+                  constraints: BoxConstraints(maxWidth: 215),
                   child: Column(
-                    // Column hinzugefügt
                     children: [
-                      Padding(
-                        padding: const EdgeInsets.only(top: 16.0),
-                        child: Material(
-                          elevation: 4,
-                          borderRadius: BorderRadius.circular(4),
-                          child: Image.asset(
-                            // Bild für die erste Card
-                            'assets/images/athen_home.jpg', // Pfad für das Athen-Bild anpassen
-                            width: 180,
-                            fit: BoxFit.cover,
-                          ),
+                      Card(
+                        color: Color.fromRGBO(243, 239, 231, 1.0),
+                        elevation: 10,
+                        margin: EdgeInsets.symmetric(vertical: 8),
+                        child: Column(
+                          children: [
+                            Padding(
+                              padding: const EdgeInsets.only(top: 16.0),
+                              child: Material(
+                                elevation: 4,
+                                borderRadius: BorderRadius.circular(4),
+                                child: Image.asset(
+                                  'assets/images/athen_home.jpg',
+                                  width: 180,
+                                  fit: BoxFit.cover,
+                                ),
+                              ),
+                            ),
+                            ListTile(
+                              title: Text(
+                                '80 Jahre Kriegsende in Athen',
+                                textAlign: TextAlign.center,
+                                style: TextStyle(
+                                  fontWeight: FontWeight.bold,
+                                  fontFamily: 'SFPro',
+                                  fontSize: 14,
+                                ),
+                              ),
+                            ),
+                            Padding(
+                              padding: const EdgeInsets.only(bottom: 8.0),
+                              child: Material(
+                                elevation: 4,
+                                borderRadius: BorderRadius.circular(4),
+                                child: Image.asset(
+                                  'assets/icons/more_info.png',
+                                  width: 76,
+                                  height: 26,
+                                ),
+                              ),
+                            ),
+                            SizedBox(height: 8),
+                          ],
                         ),
                       ),
-                      ListTile(
-                        title: Text(
-                          '80 Jahre Kriegsende in Athen',
-                          textAlign: TextAlign.center,
-                          style: TextStyle(
-                            fontWeight: FontWeight.bold,
-                            fontFamily: 'SFPro',
-                            fontSize: 14,
-                          ),
-                        ),
-                      ),
-                      Material(
-                        elevation: 4,
-                        borderRadius: BorderRadius.circular(4),
-                        child: Image.asset(
-                          'assets/icons/more_info.png',
-                          width: 76,
-                          height: 26,
+                      Card(
+                        color: Color.fromRGBO(243, 239, 231, 1.0),
+                        elevation: 10,
+                        margin: EdgeInsets.symmetric(vertical: 8),
+                        child: Column(
+                          children: [
+                            Padding(
+                              padding: const EdgeInsets.only(top: 16.0),
+                              child: Material(
+                                elevation: 4,
+                                borderRadius: BorderRadius.circular(4),
+                                child: Image.asset(
+                                  'assets/images/thessaloniki_home.jpg',
+                                  width: 180,
+                                  fit: BoxFit.cover,
+                                ),
+                              ),
+                            ),
+                            ListTile(
+                              title: Text(
+                                '80 Jahre Kriegsende in Thessalonikki',
+                                textAlign: TextAlign.center,
+                                style: TextStyle(
+                                  fontWeight: FontWeight.bold,
+                                  fontFamily: 'SFPro',
+                                  fontSize: 14,
+                                ),
+                              ),
+                            ),
+                            Padding(
+                              padding: const EdgeInsets.only(bottom: 8.0),
+                              child: Material(
+                                elevation: 4,
+                                borderRadius: BorderRadius.circular(4),
+                                child: Image.asset(
+                                  'assets/icons/more_info.png',
+                                  width: 76,
+                                  height: 26,
+                                ),
+                              ),
+                            ),
+                            SizedBox(height: 8),
+                          ],
                         ),
                       ),
                     ],
                   ),
                 ),
               ),
-              SizedBox(
-                width: 204,
-                height: 250,
-                child: Card(
-                  color: Color.fromRGBO(243, 239, 231, 1.0),
-                  elevation: 10,
-                  margin: EdgeInsets.fromLTRB(16, 4, 16, 8),
-                  child: Column(
-                    // Column hinzugefügt
-                    children: [
-                      Padding(
-                        padding: const EdgeInsets.only(top: 16.0),
-                        child: Material(
-                          elevation: 4,
-                          borderRadius: BorderRadius.circular(4),
-                          child: Image.asset(
-                            // Bild für die zweite Card
-                            'assets/images/thessaloniki_home.jpg', // URL für das Thessaloniki-Bild anpassen
-                            width: 180,
-                            fit: BoxFit.cover,
-                          ),
-                        ),
-                      ),
-                      ListTile(
-                        title: Text(
-                          '80 Jahre Kriegsende in Thessalonikki',
-                          textAlign: TextAlign.center,
-                          style: TextStyle(
-                            fontWeight: FontWeight.bold,
-                            fontFamily: 'SFPro',
-                            fontSize: 14,
-                          ),
-                        ),
-                      ),
-
-                      Material(
-                        elevation: 4,
-                        borderRadius: BorderRadius.circular(4),
-                        child: Image.asset(
-                          'assets/icons/more_info.png',
-                          width: 76,
-                          height: 26,
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-              ),
-
-              SizedBox(
+              SizedBox(height: 16),
+              Center(
                 child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  mainAxisAlignment: MainAxisAlignment.center,
                   children: <Widget>[
-                    SizedBox(
+                    Container(
                       width: 167,
-                      height: 102,
+                      margin: EdgeInsets.only(right: 16),
                       child: Card(
                         color: Color.fromRGBO(243, 239, 231, 1.0),
                         elevation: 10,
-                        margin: EdgeInsets.only(bottom: 8),
-                        child: Column(
-                          // Column hinzugefügt
-                          children: [
-                            ListTile(
-                              title: Text(
+                        margin: EdgeInsets.only(bottom: 16),
+                        child: Padding(
+                          padding: const EdgeInsets.fromLTRB(8, 4, 8, 8),
+                          child: Column(
+                            mainAxisSize: MainAxisSize.min,
+                            crossAxisAlignment: CrossAxisAlignment.center,
+                            children: [
+                              Text(
                                 'TIMELINE',
                                 textAlign: TextAlign.center,
                                 style: TextStyle(
@@ -162,49 +164,51 @@ class MyApp extends StatelessWidget {
                                   fontSize: 14,
                                 ),
                               ),
-                            ),
-                            Padding(
-                              padding: const EdgeInsets.symmetric(
-                                horizontal: 8.0,
-                              ),
-                              child: Text(
-                                'Hier finden sie alle historischen Ereignisse in Griechenland von 1941 bis 1945',
-                                textAlign: TextAlign.center,
-                                style: TextStyle(
-                                  fontSize: 11,
-                                  height: 1.02,
-                                  fontWeight: FontWeight.w600,
-
-                                  color: Color.fromRGBO(84, 95, 113, 1.0),
-                                  fontFamily: 'SFPro',
+                              Padding(
+                                padding: const EdgeInsets.symmetric(
+                                  horizontal: 8.0,
+                                ),
+                                child: Text(
+                                  'Hier finden sie alle historischen Ereignisse in Griechenland von 1941 bis 1945.',
+                                  textAlign: TextAlign.center,
+                                  style: TextStyle(
+                                    fontSize: 11,
+                                    height: 1.2,
+                                    fontWeight: FontWeight.w600,
+                                    color: Color.fromRGBO(84, 95, 113, 1.0),
+                                    fontFamily: 'SFPro',
+                                  ),
                                 ),
                               ),
-                            ),
-                            Material(
-                              elevation: 4,
-                              borderRadius: BorderRadius.circular(4),
-                              child: Image.asset(
-                                'assets/icons/more_info.png',
-                                width: 76,
-                                height: 26,
+                              const SizedBox(height: 8),
+                              Material(
+                                elevation: 4,
+                                borderRadius: BorderRadius.circular(4),
+                                child: Image.asset(
+                                  'assets/icons/more_info.png',
+                                  width: 76,
+                                  height: 26,
+                                ),
                               ),
-                            ),
-                          ],
+                            ],
+                          ),
                         ),
                       ),
                     ),
-                    SizedBox(
+                    Container(
                       width: 167,
-                      height: 102,
+                      margin: EdgeInsets.only(right: 16),
                       child: Card(
                         color: Color.fromRGBO(243, 239, 231, 1.0),
                         elevation: 10,
                         margin: EdgeInsets.only(bottom: 16),
-                        child: Column(
-                          // Column hinzugefügt
-                          children: [
-                            ListTile(
-                              title: Text(
+                        child: Padding(
+                          padding: const EdgeInsets.fromLTRB(8, 4, 8, 8),
+                          child: Column(
+                            mainAxisSize: MainAxisSize.min,
+                            crossAxisAlignment: CrossAxisAlignment.center,
+                            children: [
+                              Text(
                                 'KARTE',
                                 textAlign: TextAlign.center,
                                 style: TextStyle(
@@ -213,43 +217,43 @@ class MyApp extends StatelessWidget {
                                   fontSize: 14,
                                 ),
                               ),
-                            ),
-                            Padding(
-                              padding: const EdgeInsets.symmetric(
-                                horizontal: 8.0,
-                              ),
-                              child: Text(
-                                'Hier finden sie unsere interaktive Karte auf der alle Lagerstandorte verzeichnet sind.',
-                                textAlign: TextAlign.center,
-                                style: TextStyle(
-                                  fontSize: 11,
-                                  height: 1.02,
-                                  fontWeight: FontWeight.w600,
-                                  color: Color.fromRGBO(84, 95, 113, 1.0),
-                                  fontFamily: 'SFPro',
+                              Padding(
+                                padding: const EdgeInsets.symmetric(
+                                  horizontal: 8.0,
+                                ),
+                                child: Text(
+                                  'Hier finden sie unsere Karte auf der alle Lagerstandorte verzeichnet sind.',
+                                  textAlign: TextAlign.center,
+                                  style: TextStyle(
+                                    fontSize: 11,
+                                    height: 1.2,
+                                    fontWeight: FontWeight.w600,
+                                    color: Color.fromRGBO(84, 95, 113, 1.0),
+                                    fontFamily: 'SFPro',
+                                  ),
                                 ),
                               ),
-                            ),
-                            Material(
-                              elevation: 4,
-                              borderRadius: BorderRadius.circular(4),
-                              child: Image.asset(
-                                'assets/icons/more_info.png',
-                                width: 76,
-                                height: 26,
+                              const SizedBox(height: 8),
+                              Material(
+                                elevation: 4,
+                                borderRadius: BorderRadius.circular(4),
+                                child: Image.asset(
+                                  'assets/icons/more_info.png',
+                                  width: 76,
+                                  height: 26,
+                                ),
                               ),
-                            ),
-                          ],
+                            ],
+                          ),
                         ),
                       ),
                     ),
                   ],
                 ),
               ),
-            ], // Children der ListView
+            ],
           ),
         ),
-
         bottomNavigationBar: BottomNavigationBar(
           type: BottomNavigationBarType.fixed,
           showSelectedLabels: false,
